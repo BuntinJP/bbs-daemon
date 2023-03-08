5fc9bfac7b8277133589d82ad25e1e285e9b38d5
buntin-alien <mail.buntin.tech>
2023-03-09 08:38:50 +0900

    webpackバンどらー整備

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+module.exports = {
+    mode: 'development',
+    entry: path.join(__dirname, 'src', 'index.tsx'),
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                // 拡張子 .ts もしくは .tsx の場合
+                test: /\.tsx?$/,
+                // TypeScript をコンパイルする
+                use: 'ts-loader',
+            },
+            {
+                test: /\.(sass|less|css)$/,
+                use: ['style-loader', 'css-loader', 'less-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js'],
+    },
+    target: ['web', 'es5'],
+};
