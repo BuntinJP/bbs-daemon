c670cb6337f6ab3a70b23097938ea843a08caa0d
buntin-alien <mail.buntin.tech>
2023-03-09 08:07:56 +0900

    Ziped the stash

--- stash/server.ts
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-import mysql2 from 'mysql2';
-import express from 'express';
-const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
-
-const app = express();
-app.listen(portNum, () => {
-    console.log(`Listening on port ${portNum}`);
-});
--- stash/src/component/FormComp.tsx
+++
@@ -1,5 +0,0 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import request from 'superagent';
-import { Button } from 'react-bootstrap';
-import Dialog from 'react-bootstrap-dialog';
