420a1ea6d0875db2f7593e20e82c5ed099f23666
buntin-alien <mail.buntin.tech>
2023-03-09 08:07:03 +0900

    クライアントサイド完成

---
+++ server.ts
@@ -0,0 +1,13 @@
+import express, { Express, Request, Response } from 'express';
+
+const app: Express = express();
+
+const port = 4000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log('Server is running on port ' + port);
+});
