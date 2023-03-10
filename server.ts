ee85268a78cb98f2bf5a3fa47e74c45a6e429aae
Buntin-Synthia <aokitakumi72@gmail.com>
2023-03-10 17:18:15 +0900

    t

--- server.ts
+++ server.ts
@@ -2,15 +2,15 @@ import express, { Express, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import mysql from 'mysql2';
 import { loadTomlSettings } from './util';
-/* 
-a
-a
-*/
+
 interface Config {
     host: string;
     user: string;
     password: string;
     database: string;
+}
+interface Outdata {
+    [key: string]: any;
 }
 
 // express server configuration
@@ -19,8 +19,9 @@ const portNum = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const config: Config = loadTomlSettings('settings.toml');
+const config: Config = loadTomlSettings('config.toml');
 app.listen(portNum, () => {
+    console.log(app.name);
     console.log(
         'Server is started',
         `http://localhost:${portNum}`,
@@ -28,11 +29,11 @@ app.listen(portNum, () => {
     );
 });
 
-app.get('/api', (req, res) => {
+app.get('/api', (_req: Request, res: Response) => {
     console.log('取得');
     let c = mysql.createConnection(config);
     c.connect();
-    c.query('select * from log', (error, results) => {
+    c.query('select * from log', (error, results: mysql.RowDataPacket[]) => {
         if (error) {
             sendJSON(res, false, { logs: [], msg: error });
             return;
@@ -48,40 +49,45 @@ app.post('/api', (req, res) => {
     console.log('書き込み');
     const b = req.body;
     let writerName = b.name !== '' ? b.name : '名無しさん';
+    if (b.body === '') {
+        return;
+    }
     let c = mysql.createConnection(config);
-    c.query('select count(*) as c from log', (error, results) => {
-        if (error) throw error;
-        const id = results[0].c;
-        let d = new Date();
-        //[ "日", "月", "火", "水", "木", "金", "土" ][dayOfWeek]
-        let date = `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}(${
-            ['日', '月', '火', '水', '木', '金', '土'][d.getDay()]
-        })${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`.replace(
-            /\n|\r/g,
-            ''
-        );
-        c.query(
-            'INSERT INTO log VALUES (?, ?, ?, ?)',
-            [id, writerName, b.body, date],
-            (error, results) => {
-                if (error) {
-                    console.error(error);
-                    sendJSON(res, false, { msg: error });
+    c.query(
+        'select count(*) as c from log',
+        (error, results: mysql.RowDataPacket[]) => {
+            if (error) throw error;
+            const id: number = results[0].c;
+            let d = new Date();
+            let date = `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}(${
+                ['日', '月', '火', '水', '木', '金', '土'][d.getDay()]
+            })${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`.replace(
+                /\n|\r/g,
+                ''
+            );
+            c.query(
+                'INSERT INTO log VALUES (?, ?, ?, ?)',
+                [id, writerName, b.body, date],
+                (error, _results) => {
+                    if (error) {
+                        console.error(error);
+                        sendJSON(res, false, { msg: error });
+                        c.end();
+                    }
+                    sendJSON(res, true, {});
                     c.end();
+                    console.log('書き込み完了');
                 }
-                sendJSON(res, true, {});
-                c.end();
-                console.log('書き込み完了');
-            }
-        );
-    });
+            );
+        }
+    );
 });
 
-app.use((req, res) => {
+app.use((_req, res) => {
     res.status(404);
     express.static('./public/404.html');
 });
-const sendJSON = (res, result, obj) => {
+const sendJSON = (res: express.Response, result: boolean, obj: Outdata) => {
     obj['result'] = result;
     res.json(obj);
 };
--- util.ts
+++ util.ts
@@ -2,8 +2,10 @@ import toml from 'toml';
 import fs from 'fs';
 import { Config } from './server';
 
-export const loadTomlSettings = (path: string) => {
+const loadTomlSettings = (path: string) => {
     return JSON.parse(
         JSON.stringify(toml.parse(fs.readFileSync(path).toString()))
     ) as Config;
 };
+
+export { loadTomlSettings };
